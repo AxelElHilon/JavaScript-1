4d1f8550d675d24607b525ae1b4f031c8976cc5c
Axel <axelelhilon@gmail.com>
2024-12-10 01:16:20 -0300

    Primer Commit

---
+++ clase 1/start.js
@@ -0,0 +1,20 @@
+
+//   let edad = 97 //number
+//   let nombre = "Axel" //string
+
+//   const pi = 3.14 
+
+//   let num1 = 3
+//   let num2 = 17
+
+//   let result = num1+num2
+
+
+// console.log("el resultado de la suma es:"+ result)
+
+//   alert/ //cuadro de dialogo 
+//   console.log // consola (herramienta del desarrolador)
+
+let nombres = prompt("Ingresa tu nombre")
+
+alert("Bienvenido "+ nombres)
---
+++ clase 2/start.js
@@ -0,0 +1,9 @@
+
+
+let usuario = prompt("Cual es tu nombre?")
+
+if (usuario == "Axel") {
+    alert("Bienvenido Axel")
+} else {
+    alert("Vos no sos Axel gato!")
+}
