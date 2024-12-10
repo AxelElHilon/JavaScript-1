12c3585235627884ff0e98b43113334f165508ad
Axel <axelelhilon@gmail.com>
2024-12-10 01:55:53 -0300

    A Descansar

--- clase 2/start.js
+++ clase 2/start.js
@@ -1,9 +1,16 @@
 
 
-let usuario = prompt("Cual es tu nombre?").toUpperCase()
+// let usuario = prompt("Cual es tu nombre?").toUpperCase()
 
-if (usuario == "AXEL") {
-    alert("Bienvenido Axel")
-} else {
-    alert("Vos no sos Axel gato!")
+// if (usuario == "AXEL") {
+//     alert("Bienvenido Axel")
+// } else {
+//     alert("Vos no sos Axel gato!")
+// }
+
+
+let numero =  parseInt(prompt("Ingresa un numero"))
+
+if (numero === 15){
+    alert("coinciden")
 }
