c38b9297ffbcb7baedb261579e1b5429421fc2e3
Axel <axelelhilon@gmail.com>
2024-12-11 01:38:49 -0300

    Estilizado

---
+++ clase2/start.js
@@ -0,0 +1,32 @@
+
+
+// let usuario = prompt("Cual es tu nombre?").toUpperCase()
+
+// if (usuario == "AXEL") {
+//     alert("Bienvenido Axel")
+// } else {
+//     alert("Vos no sos Axel gato!")
+// }
+
+
+// let numero =  parseInt(prompt("Ingresa un numero"))
+
+// if (numero === 15){
+//     alert("coinciden")
+// }
+
+// let unColor = "verde"
+
+// if (unColor == "Rojo"){
+//     console.log("el color es Rojo");
+// }else{
+//     console.log("el color NO es Rojo");
+// 
+
+let nombreUsuario = prompt ("Ingresar nombre de usuario")
+
+if(nombreUsuario == ""){
+    alert("No ingresaste nombre de usuario")
+}else{
+    alert("Nombre de usuario ingresado " + nombreUsuario);
+}
