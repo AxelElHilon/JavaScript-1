fb2630338675a4e6f7c40581f08b820645a7794f
Axel <axelelhilon@gmail.com>
2024-12-16 02:09:27 -0300

    Clase 4 Funciones

---
+++ clase4/main.js
@@ -0,0 +1,19 @@
+
+
+//   function saludar(){
+//     let nombre= prompt("Ingresa tu nombre")
+//     alert("Bienvenido ", nombre)
+//     confirm("Deseas comprar? ")
+//   }
+
+//   saludar();
+
+
+
+  //Funcion con Parametros
+
+  function saludar(nombre){
+    console.log("Hola ", nombre)
+  }
+
+  saludar("axel");
